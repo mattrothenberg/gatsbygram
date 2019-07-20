111ba5741406032e350ad9714719c6ee28680ec7
Matt Rothenberg <mattrothenberg@gmail.com>
2019-07-20 16:18:00 -0400

    Build edges with DatoCMS data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`, `gatsby-plugin-transition-link`]
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+        apiUrl: "https://site-api.datocms.com"
+      }
+    }
+  ]
 };
