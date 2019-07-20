d89f21d617c50578bdd895faef2fc41d706251bc
Matt Rothenberg <mattrothenberg@gmail.com>
2019-07-20 11:22:53 -0400

    Hook up TransitionLink library

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,15 +1,31 @@
 import React from "react";
+import TransitionLink, { TransitionState } from "gatsby-plugin-transition-link";
 
 import Layout from "../components/layout";
 import ProjectHeader from "../components/project-header";
 import ProjectContent from "../components/project-content";
 
-const Project = ({ pageContext: project }) => (
-  <Layout>
-    <ProjectHeader project={project} />
-    <ProjectContent />
-    <ProjectHeader project={project.next} truncated />
-  </Layout>
-);
+const ProjectInner = ({ transitionStatus, project }) => {
+  const nextProjectUrl = `/projects/${project.next.slug}`;
+  return (
+    <Layout>
+      <ProjectHeader project={project} />
+      <ProjectContent />
+      <TransitionLink to={nextProjectUrl}>
+        <ProjectHeader project={project.next} truncated />
+      </TransitionLink>
+    </Layout>
+  );
+};
+
+const Project = ({ pageContext: project }) => {
+  return (
+    <TransitionState>
+      {({ transitionStatus }) => (
+        <ProjectInner transitionStatus={transitionStatus} project={project} />
+      )}
+    </TransitionState>
+  );
+};
 
 export default Project;
