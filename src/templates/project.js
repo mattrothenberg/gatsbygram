760323236f934da79fd32b76dfe3bb4dc5401282
Matt Rothenberg <mattrothenberg@gmail.com>
2019-07-20 12:14:14 -0400

    Implement sliding header transition

--- src/templates/project.js
+++ src/templates/project.js
@@ -12,17 +12,44 @@ const FadingContent = posed.div({
   exiting: { opacity: 0 }
 });
 
+const SlidingHeader = posed.div({
+  exiting: {
+    y: ({ element }) => {
+      const navbar = document.querySelector("header");
+      const navbarDimensions = navbar.getBoundingClientRect();
+      const distanceToTop =
+        element.getBoundingClientRect().top - navbarDimensions.height;
+      return distanceToTop * -1;
+    },
+    transition: {
+      ease: [0.59, 0.01, 0.28, 1],
+      delay: 250,
+      duration: TRANSITION_LENGTH * 1000 - 250
+    }
+  }
+});
+
 const ProjectInner = ({ transitionStatus, project }) => {
   const nextProjectUrl = `/projects/${project.next.slug}`;
+  const shouldTruncate = ["entering", "entered"].includes(transitionStatus);
 
   const exitTransition = {
     length: TRANSITION_LENGTH,
-    trigger: () => console.log("We are exiting")
+    trigger: () => {
+      if (document) {
+        document.body.style.overflow = "hidden";
+      }
+    }
   };
 
   const entryTransition = {
     delay: TRANSITION_LENGTH,
-    trigger: () => console.log("We are entering")
+    trigger: () => {
+      if (document && window) {
+        window.scrollTo(0, 0);
+        document.body.style.overflow = "visible";
+      }
+    }
   };
 
   return (
@@ -32,11 +59,17 @@ const ProjectInner = ({ transitionStatus, project }) => {
         <ProjectContent />
       </FadingContent>
       <TransitionLink
+        style={{
+          textDecoration: "none",
+          color: "inherit"
+        }}
         to={nextProjectUrl}
         exit={exitTransition}
         entry={entryTransition}
       >
-        <ProjectHeader project={project.next} truncated />
+        <SlidingHeader pose={transitionStatus}>
+          <ProjectHeader project={project.next} truncated={shouldTruncate} />
+        </SlidingHeader>
       </TransitionLink>
     </Layout>
   );
