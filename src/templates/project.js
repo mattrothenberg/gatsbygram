36e6b4656e4c2f6589f7e4ea4544b996075654fc
Matt Rothenberg <mattrothenberg@gmail.com>
2019-07-20 11:39:10 -0400

    Implement fading content transition

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,17 +1,41 @@
 import React from "react";
 import TransitionLink, { TransitionState } from "gatsby-plugin-transition-link";
+import posed from "react-pose";
 
 import Layout from "../components/layout";
 import ProjectHeader from "../components/project-header";
 import ProjectContent from "../components/project-content";
 
+const TRANSITION_LENGTH = 1.5;
+
+const FadingContent = posed.div({
+  exiting: { opacity: 0 }
+});
+
 const ProjectInner = ({ transitionStatus, project }) => {
   const nextProjectUrl = `/projects/${project.next.slug}`;
+
+  const exitTransition = {
+    length: TRANSITION_LENGTH,
+    trigger: () => console.log("We are exiting")
+  };
+
+  const entryTransition = {
+    delay: TRANSITION_LENGTH,
+    trigger: () => console.log("We are entering")
+  };
+
   return (
     <Layout>
-      <ProjectHeader project={project} />
-      <ProjectContent />
-      <TransitionLink to={nextProjectUrl}>
+      <FadingContent pose={transitionStatus}>
+        <ProjectHeader project={project} />
+        <ProjectContent />
+      </FadingContent>
+      <TransitionLink
+        to={nextProjectUrl}
+        exit={exitTransition}
+        entry={entryTransition}
+      >
         <ProjectHeader project={project.next} truncated />
       </TransitionLink>
     </Layout>
